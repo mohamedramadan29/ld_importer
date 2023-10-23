f7cf5289036a4e836adee8a15744412aea37ea2c
mohamed <mr319242@gmail.com>
2023-10-24 00:59:07 +0300

    update

--- include/header.php
+++ include/header.php
@@ -12,9 +12,9 @@
 	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $fonts ?>/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link  rel="stylesheet" type="text/css" href="<?php echo $fonts ?>/font-awesome-4.7.0/css/font-awesome.min.css">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $fonts ?>/iconic/css/material-design-iconic-font.min.css">
+	<link  rel="stylesheet" type="text/css" href="<?php echo $fonts ?>/iconic/css/material-design-iconic-font.min.css">
 	<!--=============================================================================================== 
 	<link rel="stylesheet" type="text/css" href="<?php echo $fonts ?>/linearicons-v1.0.0/icon-font.min.css">
 -->
@@ -26,23 +26,23 @@
 	<!-- ============================================================================================== -->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	
+	<link  rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+
 	<!--===============================================================================================
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
 	-->
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+	<link  rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/MagnificPopup/magnific-popup.css">
+	<link  rel="stylesheet" type="text/css" href="vendor/MagnificPopup/magnific-popup.css">
 	<!--===============================================================================================
 	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
 	-->
-	    <!-- START NICE VIDEO  -->
-		<link rel="stylesheet" href="<?php echo $css; ?>plyr.min.css" />
-    <!-- END NICE VIDEO  -->
+	<!-- START NICE VIDEO  -->
+	<link  rel="stylesheet" href="<?php echo $css; ?>plyr.min.css" />
+	<!-- END NICE VIDEO  -->
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $css; ?>/util.css">
+	<link  rel="stylesheet" type="text/css" href="<?php echo $css; ?>/util.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo $css; ?>/main-v1.css">
 	<!--===============================================================================================-->
 </head>
